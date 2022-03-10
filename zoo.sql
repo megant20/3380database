e776a863eb6df62ef2ef82445b9dbd1acfdd0e0a
Megan Tucker <tuckermegan123@gmail.com>
2022-03-09 20:00:23 -0600

    Update zoo.sql

--- zoo.sql
+++ zoo.sql
@@ -2,6 +2,27 @@ create database zoo;
 
 CREATE TABLE location (
     locationID INT NOT NULL AUTO_INCREMENT,
-    title varchar(255)
+    title varchar(30),
+    PRIMARY KEY (`locationID`)
 );
+
+CREATE TABLE department (
+    departmentID INT NOT NULL AUTO_INCREMENT,
+    title varchar(30),
+    PRIMARY KEY (departmentID)
+);
+
+CREATE TABLE attractions (
+    attractionID INT NOT NULL AUTO_INCREMENT,
+    locationID varchar(30),
+    purchases DECIMAL(15, 2) NOT NULL DEFAULT 0,
+    PRIMARY KEY (attractionID)
+    FOREIGN KEY (locationID) REFERENCES location(locationID)
+);
+
+
+
+
+
+
 
