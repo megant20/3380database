66a5fca725bdcf22edf69ce4c41b384154eef539
Secretman33 <100887388+Secretman33@users.noreply.github.com>
2022-03-30 00:39:41 -0500

    Update zoo.sql

--- zoo.sql
+++ zoo.sql
@@ -1,24 +1,19 @@
-CREATE DATABASE IF NOT EXISTS zoo;
+DROP DATABASE IF EXISTS zoo;
+CREATE DATABASE zoo;
 
 USE zoo;
-
-DROP TABLE IF EXISTS location;
 
 CREATE TABLE location (
     locationID INT NOT NULL AUTO_INCREMENT,
     title varchar(30),
     PRIMARY KEY (locationID)
-)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS department;
+);
 
 CREATE TABLE department (
     departmentID INT NOT NULL AUTO_INCREMENT,
     title varchar(30),
     PRIMARY KEY (departmentID)
-)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS manager;
+);
 
 CREATE TABLE manager (
     departmentID INT NOT NULL,
@@ -30,9 +25,7 @@ CREATE TABLE manager (
     PRIMARY KEY (managerID, ssn),
     CONSTRAINT manager_ibfk_1 FOREIGN KEY (departmentID) REFERENCES department (departmentID),
     CONSTRAINT manager_ibfk_2 FOREIGN KEY (locationID) REFERENCES location (locationID)
-)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS attraction;
+);
 
 CREATE TABLE attraction (
     attractionID INT NOT NULL AUTO_INCREMENT,
@@ -40,22 +33,18 @@ CREATE TABLE attraction (
     purchases DECIMAL(15, 2) NOT NULL DEFAULT 0,
     PRIMARY KEY (attractionID),
     CONSTRAINT attraction_ibfk_1 FOREIGN KEY (locationID) REFERENCES location (locationID)
-)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS animal;
+);
 
 CREATE TABLE animal (
     animalID INT NOT NULL AUTO_INCREMENT,
     attractionID INT NOT NULL,
     name varchar(30),
-    species varchar(15),
-    gender varchar(15),
+    class ENUM('Mammal','Bird','Amphibian','Fish','Reptile','Invertebrate'),
+    gender ENUM('Male','Female','Other'),
     DOB varchar(8), ##do more with this later
     PRIMARY KEY (animalID),
     CONSTRAINT animal_ibfk_1 FOREIGN KEY (attractionID) REFERENCES attraction (attractionID)
-)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS employee;
+);
 
 CREATE TABLE employee (
     attractionID INT NOT NULL,
@@ -69,18 +58,14 @@ CREATE TABLE employee (
     CONSTRAINT employee_ibfk_1 FOREIGN KEY (attractionID) REFERENCES attraction (attractionID),
     CONSTRAINT employee_ibfk_2 FOREIGN KEY (locationID) REFERENCES location (locationID),
     CONSTRAINT employee_ibfk_3 FOREIGN KEY (managerID) REFERENCES manager (managerID)
-)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS gift_shop;
+);
 
 CREATE TABLE gift_shop (
     giftshopID INT NOT NULL AUTO_INCREMENT,
     donations DECIMAL(15,2) NOT NULL DEFAULT 0,
     product_sales DECIMAL(15,2) NOT NULL DEFAULT 0,
     PRIMARY KEY (giftshopID)
-)   ENGINE=InnoDB DEFAULT CHARSET=latin1;
-    
-DROP TABLE IF EXISTS user;
+);
 
 CREATE TABLE user (
     user_name varchar(30),
@@ -88,7 +73,7 @@ CREATE TABLE user (
     age INT,
     phone_number varchar(10) NOT NULL, ##do more with this later
     email varchar(45) NOT NULL,
-    gender varchar(15),
+    gender ENUM('Male','Female','Other'),
     tickets_purchased INT NOT NULL DEFAULT 0,
     PRIMARY KEY (phone_number, email)
- )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ );
