46d3533ffe50489ebfc7dcb89c09cd37f99dc6db
Secretman33 <100887388+Secretman33@users.noreply.github.com>
2022-04-04 21:42:10 -0500

    Update zoo.sql

--- zoo.sql
+++ zoo.sql
@@ -8,12 +8,24 @@ CREATE TABLE location (
     title varchar(30),
     PRIMARY KEY (locationID)
 );
+INSERT INTO location(title) VALUES 
+('African Forest'),
+('Primate Land'),
+('Feline Enclosures'),
+('Wetlands'),
+('South American Jungle');
 
 CREATE TABLE department (
     departmentID INT NOT NULL AUTO_INCREMENT,
     title varchar(30),
     PRIMARY KEY (departmentID)
 );
+INSERT INTO department(title) VALUES
+('Maintenance'),
+('Zoology'),
+('Veterinary'),
+('Marketing'),
+('Tourism');
 
 CREATE TABLE manager (
     departmentID INT NOT NULL,
@@ -21,12 +33,23 @@ CREATE TABLE manager (
     name varchar(30) NOT NULL,
     managerID INT NOT NULL AUTO_INCREMENT,
     ssn INT NOT NULL,
-    data_hired varchar(10),
+    date_hired varchar(10),
     age INT,
     PRIMARY KEY (managerID, ssn),
     CONSTRAINT manager_ibfk_1 FOREIGN KEY (departmentID) REFERENCES department (departmentID),
     CONSTRAINT manager_ibfk_2 FOREIGN KEY (locationID) REFERENCES location (locationID)
 );
+INSERT INTO manager(name,ssn,date_hired,age,departmentID,locationID) VALUES 
+	('John Smith',123456789,'01/01/2000',40,
+		(SELECT departmentID FROM department WHERE title ='Maintenance'),(SELECT locationID FROM location WHERE title = 'African Forest')),
+	('Jane Lowry',987654321,'10/10/2020',20,
+		(SELECT departmentID FROM department WHERE title ='Zoology'),(SELECT locationID FROM location WHERE title = 'Primate Land')),
+	('Aaron Withers',401912318,'05/15/2015',29,
+		(SELECT departmentID FROM department WHERE title ='Marketing'),(SELECT locationID FROM location WHERE title = 'Feline Enclosures')),
+	('Luke Berry',121495150,'12/04/2021',35,
+		(SELECT departmentID FROM department WHERE title ='Veterinary'),(SELECT locationID FROM location WHERE title = 'Wetlands')),
+	('Rebecca Lux',157903218,'02/18/2012',48,
+		(SELECT departmentID FROM department WHERE title ='Tourism'),(SELECT locationID FROM location WHERE title = 'South American Jungle'));	
 
 CREATE TABLE attraction (
     attractionID INT NOT NULL AUTO_INCREMENT,
@@ -35,6 +58,14 @@ CREATE TABLE attraction (
     PRIMARY KEY (attractionID),
     CONSTRAINT attraction_ibfk_1 FOREIGN KEY (locationID) REFERENCES location (locationID)
 );
+INSERT INTO attraction(purchases,locationID) VALUES
+	(1234.56, (SELECT locationID FROM location WHERE title ='African Forest')),
+    (426.90, (SELECT locationID FROM location WHERE title ='South American Jungle')),
+    (3424.15, (SELECT locationID FROM location WHERE title ='Feline Enclosures')),
+    (5612.54, (SELECT locationID FROM location WHERE title ='Primate Land')),
+    (15320, (SELECT locationID FROM location WHERE title ='African Forest')),
+    (234.36, (SELECT locationID FROM location WHERE title ='Wetlands')),
+    (1145.31, (SELECT locationID FROM location WHERE title ='Wetlands'));
 
 CREATE TABLE animal (
     animalID INT NOT NULL AUTO_INCREMENT,
@@ -46,6 +77,10 @@ CREATE TABLE animal (
     PRIMARY KEY (animalID),
     CONSTRAINT animal_ibfk_1 FOREIGN KEY (attractionID) REFERENCES attraction (attractionID)
 );
+	INSERT INTO animal(name,class,gender,DOB,attractionID) VALUES
+    ('Percy', 'Mammal', 'Male', '04/27/2020', (SELECT attracactionID FROM attraction WHERE title ='African Forest'));
+
+
 
 CREATE TABLE employee (
     attractionID INT NOT NULL,
@@ -54,7 +89,7 @@ CREATE TABLE employee (
     employeeID INT NOT NULL AUTO_INCREMENT,
     name varchar(30) NOT NULL,
     ssn INT NOT NULL,
-    data_hired varchar(10),
+    date_hired varchar(10),
     age INT,
     PRIMARY KEY (employeeID, ssn),
     CONSTRAINT employee_ibfk_1 FOREIGN KEY (attractionID) REFERENCES attraction (attractionID),
@@ -79,3 +114,6 @@ CREATE TABLE user (
     tickets_purchased INT NOT NULL DEFAULT 0,
     PRIMARY KEY (phone_number, email)
  );
+#SELECT title FROM location;
+#SELECT * FROM manager;
+#SELECT * FROM attraction;
